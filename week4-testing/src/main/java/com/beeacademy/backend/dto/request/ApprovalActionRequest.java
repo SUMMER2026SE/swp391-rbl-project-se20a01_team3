712a88fa6f4a7527af6dc0a5285b584f010556a8
ThanhDat <thanhdatvv05@gmail.com>
2026-06-17 00:09:08 +0700

    chore: xóa file rác và dọn dẹp week4-testing

--- week4-testing/src/main/java/com/beeacademy/backend/dto/request/ApprovalActionRequest.java
+++
@@ -1,8 +0,0 @@
-package com.beeacademy.backend.dto.request;
-
-import jakarta.validation.constraints.Size;
-
-public record ApprovalActionRequest(
-        @Size(max = 2000, message = "Nhận xét tối đa 2000 ký tự")
-        String comment
-) {}
